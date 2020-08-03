b01c0e79201ceb6151aed4a4f6d19af5303bd7b7
maykul <maykul@microsoft.com>
2020-08-03 14:03:20 -0500

    Adding CustomWhoAmI App Org Service
    Adding CustomWhoAmI App Org Service

---
+++ CustomApp/QuickStart/QuickStartCS/ErrorLog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerApps.Samples
+{
+    public static class ErrorLog
+    {
+
+        public static void Logger(string ErrorCode, string ErrorMsg)
+        {
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                path = path + "\\Logs\\";
+
+                // check if directory exists
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path = path + DateTime.Today.ToString("dd-MMM-yy") + ".log";
+                // check if file exist
+                if (!File.Exists(path))
+                {
+                    File.Create(path).Dispose();
+                }
+                // log the error now
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    string error = "\n" + DateTime.Now.ToString() + ": " + ErrorCode + ": " + ErrorMsg;
+                    writer.WriteLine(error);
+                    writer.Flush();
+                    writer.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CustomApp/QuickStart/QuickStartCS/SampleProgram.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PowerApps.Samples
+{
+   public partial class SampleProgram
+    {
+        [STAThread] // Added to support UX
+        static void Main(string[] args)
+        {
+            CrmServiceClient service = null;
+
+            try
+            {
+                service = SampleHelpers.Connect("Connect");
+                if (service.IsReady)
+                {
+                    #region Sample Code
+                    ////////////////////////////////////
+                    #region Set up
+                    SetUpSample(service);
+                    #endregion Set up
+                    #region Demonstrate
+                    // Obtain information about the logged on user from the web service.
+                    Guid userid = ((WhoAmIResponse)service.Execute(new WhoAmIRequest())).UserId;
+                    Guid orgid = ((WhoAmIResponse)service.Execute(new WhoAmIRequest())).OrganizationId;
+                    SystemUser systemUser = (SystemUser)service.Retrieve("systemuser", userid,
+                        new ColumnSet(new string[] { "firstname", "lastname" }));
+                    Console.WriteLine("Logged on user is {0} {1}.", systemUser.FirstName, systemUser.LastName);
+                    
+                    ErrorLog.Logger("Your UserId is {0}", userid.ToString());
+
+                    Console.WriteLine("Organization ID is {0}.", orgid.ToString());
+
+                    ErrorLog.Logger("Your UserId is {0}", orgid.ToString());
+
+                    // Retrieve the version of Microsoft Dynamics CRM.
+                    RetrieveVersionRequest versionRequest = new RetrieveVersionRequest();
+                    RetrieveVersionResponse versionResponse =
+                        (RetrieveVersionResponse)service.Execute(versionRequest);
+                    Console.WriteLine("Microsoft Dynamics CRM version {0}.", versionResponse.Version);
+
+                    ErrorLog.Logger("Your UserId is {0}", versionResponse.Version);
+
+
+
+                }
+                #endregion Demonstrate
+                #endregion Sample Code
+                else
+                {
+                    const string UNABLE_TO_LOGIN_ERROR = "Unable to Login to Common Data Service";
+                    if (service.LastCrmError.Equals(UNABLE_TO_LOGIN_ERROR))
+                    {
+                        Console.WriteLine("Check the connection string values in cds/App.config.");
+
+                        throw new Exception(service.LastCrmError);
+                    }
+                    else
+                    {
+                        throw service.LastCrmException;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SampleHelpers.HandleException(ex);
+            }
+
+            finally
+            {
+                if (service != null)
+                    service.Dispose();
+
+                Console.WriteLine("Press <Enter> to exit.");
+                Console.ReadLine();
+            }
+        }
+            }
+}
